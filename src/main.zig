632f631e5fc96e522f1bd2fa05898fcc1a497ba4
aidyak <yusei.kaida@giftee.co>
2026-02-07 16:18:03 +0900

    handle args

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,17 @@
 const std = @import("std");
 
-pub fn main() void {
-    std.debug.print("Hello, {s}!\n", .{"World"});
+pub fn main() !void {
+    // prepare allocator
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    // get args
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    // display args in order
+    for (args, 1..) |arg, i| {
+        std.debug.print("Argument {d}: {s}\n", .{ i, arg });
+    }
 }
